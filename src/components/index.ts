2457a41dbffae3325455541c1f28f7b5fd13f3af
zizate <giwook5600@naver.com>
2022-07-25 01:19:04 +0900

    Updates

--- src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Seo } from './seo/seo';
-export { default as Hero } from './hero/hero';
-export { default as Lazy } from './lazy/lazy';
--- src/util/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as themes, themeCookie } from './theme';
-export * from './types';
-export * from './util';
