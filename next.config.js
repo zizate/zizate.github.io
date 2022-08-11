45b2e0111b6feac3e2272b105a3c4ca53221f15d
zizate <giwook5600@naver.com>
2022-08-11 23:11:30 +0900

    수정

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ const { withContentlayer } = require("next-contentlayer");
 
 module.exports = withContentlayer({
   reactStrictMode: true,
-  // images: {
-  //   loader: "akamai",
-  //   path: "/",
-  // },
+  images: {
+    loader: "akamai",
+    path: "/",
+  },
 });
